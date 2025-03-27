3b6d103ab3adf4f3be911ad47b4c7e6af5926167
Nirajan Koirala <31283818+n7koirala@users.noreply.github.com>
2025-03-27 18:13:36 -0400

    Added logic for scaling and comm. measurement

--- psmt/tests.cpp
+++ psmt/tests.cpp
@@ -1,5 +1,16 @@
 #include "../psmt/tests.h"
 
+size_t ctxtSize(Ciphertext<DCRTPoly>& ctxt) {
+  size_t size = 0;
+  for (auto& element : ctxt->GetElements()) {
+    for (auto& subelements : element.GetAllElements()) {
+      auto length = subelements.GetLength();
+      size += length * sizeof(subelements[0]);
+    }
+  }
+  return size;
+};
+
 std::vector<double> genDataNormal(
     uint32_t numItems
 ) {
@@ -240,13 +251,17 @@ void testFullPipelineRealData(
     std::string DBPath, 
     std::string ansPath, 
     std::string paramPath, 
-    bool isSim
+    bool isSim,
+    int scalingMod
 ) {
     std::cout << "<<< " << "TEST A FULL PIPELINE WITH REAL DATA" << ">>>" << std::endl;
     FHEParams params;
     params.multiplicativeDepth = 41;
     params.ringDim = 1<<17;
-    params.scalingModSize = 50;
+
+    params.scalingModSize = scalingMod;
+    std::cout << "Scaling mod size is set to " << params.scalingModSize << std::endl;
+
     params.firstModSize = 60;
 
     FHEContext ctx = InitFHE(params);
@@ -325,6 +340,9 @@ void testFullPipelineRealData(
             cc, pk, idMsgVec[0], kappa
         );  
 
+        size_t querySize = ctxtSize(queryCtxt);
+        std::cout << "\nQuery Ciphertext Size (R -> S): " << querySize << std::endl;
+
         // Do Intersection
         std::vector<Ciphertext<DCRTPoly>> interCtxts(numVar);
         std::cout << "Do Local Server Operations" << std::endl;
@@ -333,7 +351,10 @@ void testFullPipelineRealData(
         auto t2 = std::chrono::high_resolution_clock::now();
         double tdiff = std::chrono::duration<double>(t2-t1).count();
         std::cout << "Done!" << std::endl;
-        std::cout << "Time Elapsed: " << tdiff << std::endl;        
+        std::cout << "Time Elapsed: " << tdiff << std::endl;      
+
+        size_t resultSize = ctxtSize(interCtxts[0]);
+        std::cout << "\nResult ciphertext Size (Computing Sender, S->L): " << resultSize << std::endl;  
 
         // DEBUG
         // Plaintext _ptxtDebug;
@@ -360,6 +381,9 @@ void testFullPipelineRealData(
         }
         std::cout << "Done!" << std::endl;
 
+        size_t mockSize = ctxtSize(interCtxts[1]);
+        std::cout << "\nResult ciphertext Size (Mock Senders, S->L): " << mockSize << std::endl;  
+
         // Do Logistic Regression
         std::cout << "Do Leader Server Operations" << std::endl;
         auto t1_ls = std::chrono::high_resolution_clock::now();
@@ -369,7 +393,16 @@ void testFullPipelineRealData(
         auto t2_ls = std::chrono::high_resolution_clock::now();
         double tdiff_ls = std::chrono::duration<double>(t2_ls-t1_ls).count();
         std::cout << "Done!" << std::endl;
-        std::cout << "Time Elapsed: " << tdiff_ls << std::endl;        
+        std::cout << "Time Elapsed: " << tdiff_ls << std::endl;  
+
+        auto answer = ret.evalRet;
+        // rescale this answer ciphertext ********
+        auto flag = ret.isInter;
+
+        size_t ansSize = ctxtSize(answer);
+        size_t flagSize = ctxtSize(flag);
+        std::cout << "\nReturn ciphertext size (Answer, Leader sender, L->S): " << ansSize << std::endl;       
+        std::cout << "\nReturn ciphertext size (Flag, Leader sender, L->S): " << flagSize << std::endl;        
 
     } else {
         std::cout << "ACTUAL run of the protocol for ALL servers." << std::endl;
@@ -448,18 +481,22 @@ void testFullPipelineRealData(
 }
 
 
-// Full pipeline with Compact label ciphertexts optimziation
+// Full pipeline with Compact label ciphertexts optimization
 void testFullPipelineCompactRealData(
     std::string DBPath, 
     std::string ansPath, 
     std::string paramPath, 
-    bool isSim
+    bool isSim,
+    int scalingMod
 ) {
     std::cout << "<<< " << "TEST A FULL PIPELINE WITH REAL DATA (COMPACT OPTIMIZATION)" << " >>>" << std::endl;
     FHEParams params;
     params.multiplicativeDepth = 42;
     params.ringDim = 1<<17;
-    params.scalingModSize = 50;
+
+    params.scalingModSize = scalingMod;
+    std::cout << "Scaling mod size is set to " << params.scalingModSize << std::endl;
+
     params.firstModSize = 60;
 
     FHEContext ctx = InitFHE(params);
@@ -540,6 +577,9 @@ void testFullPipelineCompactRealData(
             cc, pk, idMsgVec[0], kappa
         );  
 
+        size_t querySize = ctxtSize(queryCtxt);
+        std::cout << "\nQuery Ciphertext Size (R -> S): " << querySize << std::endl;
+
         // Do Intersection
         std::vector<Ciphertext<DCRTPoly>> interCtxts(numVar);
         std::cout << "Do Local Server Operations" << std::endl;
@@ -548,7 +588,10 @@ void testFullPipelineCompactRealData(
         auto t2 = std::chrono::high_resolution_clock::now();
         double tdiff = std::chrono::duration<double>(t2-t1).count();
         std::cout << "Done!" << std::endl;
-        std::cout << "Time Elapsed: " << tdiff << std::endl;        
+        std::cout << "Time Elapsed: " << tdiff << std::endl;     
+
+        size_t resultSize = ctxtSize(interCtxts[0]);
+        std::cout << "\nResult ciphertext Size (Computing Sender, S->L): " << resultSize << std::endl;     
 
         // Simulating the resulting ciphertexts from other senders
         std::cout << "Simulating other servers' outputs" << std::endl;
@@ -564,6 +607,9 @@ void testFullPipelineCompactRealData(
         }
         std::cout << "Done!" << std::endl;
 
+        size_t mockSize = ctxtSize(interCtxts[1]);
+        std::cout << "\nResult ciphertext Size (Mock Senders, S->L): " << mockSize << std::endl;
+
         // Do Logistic Regression
         std::cout << "Do Leader Server Operations" << std::endl;
         auto t1_ls = std::chrono::high_resolution_clock::now();
@@ -573,7 +619,15 @@ void testFullPipelineCompactRealData(
         auto t2_ls = std::chrono::high_resolution_clock::now();
         double tdiff_ls = std::chrono::duration<double>(t2_ls-t1_ls).count();
         std::cout << "Done!" << std::endl;
-        std::cout << "Time Elapsed: " << tdiff_ls << std::endl;        
+        std::cout << "Time Elapsed: " << tdiff_ls << std::endl;    
+
+        auto answer = ret.evalRet;
+        auto flag = ret.isInter;
+
+        size_t ansSize = ctxtSize(answer);
+        size_t flagSize = ctxtSize(flag);
+        std::cout << "\nReturn ciphertext size (Answer, Leader sender, L->S): " << ansSize << std::endl;       
+        std::cout << "\nReturn ciphertext size (Flag, Leader sender, L->S): " << flagSize << std::endl;               
 
     } else {
         std::cout << "ACTUAL run of the protocol for ALL servers." << std::endl;

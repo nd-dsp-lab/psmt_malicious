aa8057be3bbbab41b5c01c85eaf3a4f151bfd828
Nirajan Koirala <31283818+n7koirala@users.noreply.github.com>
2025-04-01 19:35:17 -0400

    Updated FHE params for noise-flooding

--- src/fhe_init.cpp
+++ src/fhe_init.cpp
@@ -7,20 +7,46 @@ FHEContext InitFHE(const FHEParams &params) {
     FHEContext context;
     CCParams<CryptoContextCKKSRNS> cryptoParams;
 
+
     cryptoParams.SetMultiplicativeDepth(params.multiplicativeDepth);
     cryptoParams.SetScalingModSize(params.scalingModSize);
     cryptoParams.SetFirstModSize(params.firstModSize);
     cryptoParams.SetRingDim(params.ringDim);
+    
     cryptoParams.SetScalingTechnique(FLEXIBLEAUTOEXT);
+    //cryptoParams.SetExecutionMode(EXEC_EVALUATION);
 
-    //cryptoParams.SetMultipartyMode(NOISE_FLOODING_MULTIPARTY);
+    cryptoParams.SetNoiseEstimate(39);  // max. noise is 39 for 64-bit OpenFHE
+
+    cryptoParams.SetDesiredPrecision(25);
+    cryptoParams.SetStatisticalSecurity(30);
+    cryptoParams.SetNumAdversarialQueries(1);
+
+    cryptoParams.SetDecryptionNoiseMode(NOISE_FLOODING_DECRYPT);
+    //cryptoParams.SetThresholdNumOfParties(5);
+    
+    /*
+    // int alpha = 1024;
+    // int s = 36;
+    // cryptoParams.SetNumAdversarialQueries(alpha);
+    // cryptoParams.SetStatisticalSecurity(s);
+    //cryptoParams.SetThresholdNumOfParties(ceil(num_parties/2));
+
+    // sigma (noise bits) = underroot(24 * N * alpha) * 2^(s/2), N is the ring-dimension of RLWE
+    double noise = 39;  // highest 39 for 64-bit
+    cryptoParams.SetNoiseEstimate(noise);
+    */
+    
 
     std::cout << "Generating CryptoContext..." << std::endl;
     context.cryptoContext = GenCryptoContext(cryptoParams);
+
+    std::cerr << "\nCKKS parameters :::::::: " << cryptoParams << std::endl;
     context.cryptoContext->Enable(PKE);
     context.cryptoContext->Enable(KEYSWITCH);
     context.cryptoContext->Enable(LEVELEDSHE);
     context.cryptoContext->Enable(ADVANCEDSHE);
+    context.cryptoContext->Enable(MULTIPARTY);
 
     // Generate keys.
     context.keyPair = context.cryptoContext->KeyGen();
